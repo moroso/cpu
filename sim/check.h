626e7e7ca41e7084c52ab4409e5f7192c1132348
Joshua Wise <joshua@joshuawise.com>
2014-06-08 00:50:37 -0700

    sim: check: assertion failure should have a timestamp

--- sim/check.h
+++ sim/check.h
@@ -2,9 +2,11 @@
 #define _CHECK_H
 
 #include <stdio.h>
+#include "verilated.h"
 
+extern vluint64_t main_time;
 void sim_assert_failed();
 
-#define SIM_CHECK(cond) do { if (!(cond)) { printf("%s: checker failed: %s\n", __FUNCTION__, #cond); sim_assert_failed(); } } while(0)
+#define SIM_CHECK(cond) do { if (!(cond)) { printf("%s: checker failed at %lu ns: %s\n", __PRETTY_FUNCTION__, main_time, #cond); sim_assert_failed(); } } while(0)
 
 #endif
