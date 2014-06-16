b49e524a37d86dd96fc1ad81a178ac59568b9889
Joshua Wise <joshua@joshuawise.com>
2014-06-15 23:51:59 -0700

    tb: arb: Wire out three clients to the toplevel, which we will then be able to wire up to a stim port.

--- rtl/tb/TB_MCPU_MEM_arb.v
+++ rtl/tb/TB_MCPU_MEM_arb.v
@@ -2,19 +2,32 @@ module TB_MCPU_MEM_arb(/*AUTOARG*/
    // Outputs
    ltc2mc_avl_write_req_0, ltc2mc_avl_wdata_0, ltc2mc_avl_size_0,
    ltc2mc_avl_read_req_0, ltc2mc_avl_burstbegin_0, ltc2mc_avl_be_0,
-   ltc2mc_avl_addr_0, cli2arb_stall, cli2arb_rvalid, cli2arb_rdata,
+   ltc2mc_avl_addr_0, cli2arb_rdata, cli2_stall, cli2_rvalid,
+   cli1_stall, cli1_rvalid, cli0_stall, cli0_rvalid,
    // Inputs
    ltc2mc_avl_ready_0, ltc2mc_avl_rdata_valid_0, ltc2mc_avl_rdata_0,
-   clkrst_mem_rst_n, clkrst_mem_clk, cli2arb_wdata, cli2arb_wbe,
-   cli2arb_valid, cli2arb_opcode, cli2arb_addr
+   clkrst_mem_rst_n, clkrst_mem_clk, cli2_wdata, cli2_wbe, cli2_valid,
+   cli2_opcode, cli2_addr, cli1_wdata, cli1_wbe, cli1_valid,
+   cli1_opcode, cli1_addr, cli0_wdata, cli0_wbe, cli0_valid,
+   cli0_opcode, cli0_addr
    );
 	/*AUTOINPUT*/
 	// Beginning of automatic inputs (from unused autoinst inputs)
-	input [53:0]	cli2arb_addr;		// To arb of MCPU_MEM_arb.v
-	input [5:0]	cli2arb_opcode;		// To arb of MCPU_MEM_arb.v
-	input [1:0]	cli2arb_valid;		// To arb of MCPU_MEM_arb.v
-	input [63:0]	cli2arb_wbe;		// To arb of MCPU_MEM_arb.v
-	input [511:0]	cli2arb_wdata;		// To arb of MCPU_MEM_arb.v
+	input [26:0]	cli0_addr;		// To arb of MCPU_MEM_arb.v
+	input [2:0]	cli0_opcode;		// To arb of MCPU_MEM_arb.v
+	input		cli0_valid;		// To arb of MCPU_MEM_arb.v
+	input [31:0]	cli0_wbe;		// To arb of MCPU_MEM_arb.v
+	input [255:0]	cli0_wdata;		// To arb of MCPU_MEM_arb.v
+	input [26:0]	cli1_addr;		// To arb of MCPU_MEM_arb.v
+	input [2:0]	cli1_opcode;		// To arb of MCPU_MEM_arb.v
+	input		cli1_valid;		// To arb of MCPU_MEM_arb.v
+	input [31:0]	cli1_wbe;		// To arb of MCPU_MEM_arb.v
+	input [255:0]	cli1_wdata;		// To arb of MCPU_MEM_arb.v
+	input [26:0]	cli2_addr;		// To arb of MCPU_MEM_arb.v
+	input [2:0]	cli2_opcode;		// To arb of MCPU_MEM_arb.v
+	input		cli2_valid;		// To arb of MCPU_MEM_arb.v
+	input [31:0]	cli2_wbe;		// To arb of MCPU_MEM_arb.v
+	input [255:0]	cli2_wdata;		// To arb of MCPU_MEM_arb.v
 	input		clkrst_mem_clk;		// To arb of MCPU_MEM_arb.v, ...
 	input		clkrst_mem_rst_n;	// To arb of MCPU_MEM_arb.v, ...
 	input [127:0]	ltc2mc_avl_rdata_0;	// To ltc of MCPU_MEM_ltc.v
@@ -23,9 +36,13 @@ module TB_MCPU_MEM_arb(/*AUTOARG*/
 	// End of automatics
 	/*AUTOOUTPUT*/
 	// Beginning of automatic outputs (from unused autoinst outputs)
+	output		cli0_rvalid;		// From arb of MCPU_MEM_arb.v
+	output		cli0_stall;		// From arb of MCPU_MEM_arb.v
+	output		cli1_rvalid;		// From arb of MCPU_MEM_arb.v
+	output		cli1_stall;		// From arb of MCPU_MEM_arb.v
+	output		cli2_rvalid;		// From arb of MCPU_MEM_arb.v
+	output		cli2_stall;		// From arb of MCPU_MEM_arb.v
 	output [255:0]	cli2arb_rdata;		// From arb of MCPU_MEM_arb.v
-	output [1:0]	cli2arb_rvalid;		// From arb of MCPU_MEM_arb.v
-	output [1:0]	cli2arb_stall;		// From arb of MCPU_MEM_arb.v
 	output [24:0]	ltc2mc_avl_addr_0;	// From ltc of MCPU_MEM_ltc.v
 	output [15:0]	ltc2mc_avl_be_0;	// From ltc of MCPU_MEM_ltc.v
 	output		ltc2mc_avl_burstbegin_0;// From ltc of MCPU_MEM_ltc.v
@@ -34,6 +51,7 @@ module TB_MCPU_MEM_arb(/*AUTOARG*/
 	output [127:0]	ltc2mc_avl_wdata_0;	// From ltc of MCPU_MEM_ltc.v
 	output		ltc2mc_avl_write_req_0;	// From ltc of MCPU_MEM_ltc.v
 	// End of automatics
+	
 	/*AUTOWIRE*/
 	// Beginning of automatic wires (for undeclared instantiated-module outputs)
 	wire [31:5]	arb2ltc_addr;		// From arb of MCPU_MEM_arb.v
@@ -46,7 +64,17 @@ module TB_MCPU_MEM_arb(/*AUTOARG*/
 	wire [255:0]	arb2ltc_wdata;		// From arb of MCPU_MEM_arb.v
 	// End of automatics
 	
-	MCPU_MEM_arb #(.CLIENTS(2)) 
+	/* MCPU_MEM_arb AUTO_TEMPLATE(
+		.cli2arb_stall({cli2_stall, cli1_stall, cli0_stall}),
+		.cli2arb_rvalid({cli2_rvalid, cli1_rvalid, cli0_rvalid}),
+		
+		.cli2arb_valid({cli2_valid, cli1_valid, cli0_valid}),
+		.cli2arb_opcode({cli2_opcode[2:0], cli1_opcode[2:0], cli0_opcode[2:0]}),
+		.cli2arb_addr({cli2_addr[26:0], cli1_addr[26:0], cli0_addr[26:0]}),
+		.cli2arb_wdata({cli2_wdata[255:0], cli1_wdata[255:0], cli0_wdata[255:0]}),
+		.cli2arb_wbe({cli2_wbe[31:0], cli1_wbe[31:0], cli0_wbe[31:0]}),
+		); */
+	MCPU_MEM_arb #(.CLIENTS(3), .CLIENTS_BITS(2))
 		arb(/*AUTOINST*/
 		    // Outputs
 		    .arb2ltc_valid	(arb2ltc_valid),
@@ -54,20 +82,20 @@ module TB_MCPU_MEM_arb(/*AUTOARG*/
 		    .arb2ltc_addr	(arb2ltc_addr[31:5]),
 		    .arb2ltc_wdata	(arb2ltc_wdata[255:0]),
 		    .arb2ltc_wbe	(arb2ltc_wbe[31:0]),
-		    .cli2arb_stall	(cli2arb_stall[1:0]),
+		    .cli2arb_stall	({cli2_stall, cli1_stall, cli0_stall}), // Templated
 		    .cli2arb_rdata	(cli2arb_rdata[255:0]),
-		    .cli2arb_rvalid	(cli2arb_rvalid[1:0]),
+		    .cli2arb_rvalid	({cli2_rvalid, cli1_rvalid, cli0_rvalid}), // Templated
 		    // Inputs
 		    .clkrst_mem_clk	(clkrst_mem_clk),
 		    .clkrst_mem_rst_n	(clkrst_mem_rst_n),
 		    .arb2ltc_stall	(arb2ltc_stall),
 		    .arb2ltc_rdata	(arb2ltc_rdata[255:0]),
 		    .arb2ltc_rvalid	(arb2ltc_rvalid),
-		    .cli2arb_valid	(cli2arb_valid[1:0]),
-		    .cli2arb_opcode	(cli2arb_opcode[5:0]),
-		    .cli2arb_addr	(cli2arb_addr[53:0]),
-		    .cli2arb_wdata	(cli2arb_wdata[511:0]),
-		    .cli2arb_wbe	(cli2arb_wbe[63:0]));
+		    .cli2arb_valid	({cli2_valid, cli1_valid, cli0_valid}), // Templated
+		    .cli2arb_opcode	({cli2_opcode[2:0], cli1_opcode[2:0], cli0_opcode[2:0]}), // Templated
+		    .cli2arb_addr	({cli2_addr[26:0], cli1_addr[26:0], cli0_addr[26:0]}), // Templated
+		    .cli2arb_wdata	({cli2_wdata[255:0], cli1_wdata[255:0], cli0_wdata[255:0]}), // Templated
+		    .cli2arb_wbe	({cli2_wbe[31:0], cli1_wbe[31:0], cli0_wbe[31:0]})); // Templated
 
 	MCPU_MEM_ltc ltc(/*AUTOINST*/
 			 // Outputs
