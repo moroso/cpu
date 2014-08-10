306dd34d93349196e3e209614f45af00be444263
Rick Benua <rick@benua.net>
2014-08-09 23:58:19 -0700

    mark icache signals public in testbench

--- rtl/tb/TB_MCPU_core.v
+++ rtl/tb/TB_MCPU_core.v
@@ -17,21 +17,21 @@ module TB_MCPU_core(/*AUTOARG*/
 
 	/*AUTOWIRE*/
 	// Beginning of automatic wires (for undeclared instantiated-module outputs)
-	wire [27:0]	f2ic_paddr;		// From core of MCPU_core.v
-	wire		f2ic_valid;		// From core of MCPU_core.v
+	wire [27:0]	f2ic_paddr /* verilator public */;		// From core of MCPU_core.v
+	wire		f2ic_valid /* verilator public */;		// From core of MCPU_core.v
 	wire		ft2itlb_pagefault;	// From tlb of MCPU_CACHE_tlb_dummy.v
 	wire [19:0]	ft2itlb_physpage;	// From tlb of MCPU_CACHE_tlb_dummy.v
 	wire		ft2itlb_ready;		// From tlb of MCPU_CACHE_tlb_dummy.v
 	wire		ft2itlb_valid;		// From core of MCPU_core.v
 	wire [19:0]	ft2itlb_virtpage;	// From core of MCPU_core.v
-	wire [127:0]	ic2f_packet;		// From ic of MCPU_CACHE_ic_dummy.v
-	wire		ic2f_ready;		// From ic of MCPU_CACHE_ic_dummy.v
+	wire [127:0]	ic2f_packet /* verilator public */;		// From ic of MCPU_CACHE_ic_dummy.v
+	wire		ic2f_ready /* verilator public */;		// From ic of MCPU_CACHE_ic_dummy.v
 	// End of automatics
-	
+
 	wire int_pending = 0;
 	wire [3:0] int_type = 0;
 	wire int_clear;
-	
+
 	MCPU_core core(/*AUTOINST*/
 		       // Outputs
 		       .int_clear	(int_clear),
@@ -49,7 +49,7 @@ module TB_MCPU_core(/*AUTOARG*/
 		       .ft2itlb_pagefault(ft2itlb_pagefault),
 		       .ic2f_packet	(ic2f_packet[127:0]),
 		       .ic2f_ready	(ic2f_ready));
-        
+
 	MCPU_CACHE_ic_dummy ic(/*AUTOINST*/
 			       // Outputs
 			       .ic2f_ready	(ic2f_ready),
@@ -57,9 +57,9 @@ module TB_MCPU_core(/*AUTOARG*/
 			       // Inputs
 			       .f2ic_valid	(f2ic_valid),
 			       .f2ic_paddr	(f2ic_paddr[27:0]));
-	
+
 	output wire pkt_is_break = ic2f_packet[31:0] == 32'hD1183C00;
-	
+
 	MCPU_CACHE_tlb_dummy tlb(/*AUTOINST*/
 				 // Outputs
 				 .ft2itlb_pagefault	(ft2itlb_pagefault),
