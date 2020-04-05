918f4d06e055d5d49c0c2c1b9eb71bb8a088f101
Matthew Wright <wrightm@gmail.com>
2020-04-05 13:39:50 -0700

    Add simple serial bootloader

---
+++ boot/serial_bootloader.ma
@@ -0,0 +1,80 @@
+/* Simple serial first-stage bootloader for Moroso.
+ * Does no verification or anything of the downloaded program; once
+ * it gets as many bytes as it expects, it jumps to the program to
+ * start executing it. The program is always loaded into 0x1000, and
+ * (right now) is limited to 255 packets (but that won't be a difficult
+ * limitation to remove).
+ *
+ * The protocol ("B" is bootloader, "H" is host) is:
+ *  - B -> H: "MBOOT"
+ *  - H -> B: number of packets to send (single byte)
+ *  - For each packet:
+ *    - B -> H: number of packets expected *after* this one (single byte)
+ *    - H -> B: packet contents (16 bytes)
+ *  - B -> H: "DONE"
+ *
+ * Assemble this with "mas --fmt bootrom serial_bootloader.ma -o bootrom.hex"
+ * and put bootrom.hex in the root of the repo.
+ */
+
+/* r29 will always point to the serial port's memory area.
+ * r28 is the next address to write to.
+ * r27 is the number of packets remaining to read.
+ */
+{
+  r29 <- long; long 0x80001000; // Serial port base address
+  r11 <- 0b00100; // Value to write to serial status register
+  r28 <- 0x1000; // Destination address (at which to store the program).
+                 // Note (if you change this): appears again below.
+}
+{
+  bl write_uart;
+  *l(r29 + 4) <- r11; // enable rx
+  r0 <- 'M'; }
+{ bl write_uart; r0 <- 'B'; }
+{ bl write_uart; r0 <- 'O'; }
+{ bl write_uart; r0 <- 'O'; }
+{ bl write_uart; r0 <- 'T'; }
+{ bl read_uart; }
+{ r27 <- r0; }
+outer_loop:
+  { bl read_packet; r27 <- r27 - 1; }
+  { p0 <- r27 == 0; }
+  { !p0? b outer_loop; }
+{ bl write_uart; r0 <- 'D'; }
+{ bl write_uart; r0 <- 'O'; }
+{ bl write_uart; r0 <- 'N'; }
+{ bl write_uart; r0 <- 'E'; r28 <- 0x1000; }
+{ r0 <- 0x80000000; r1 <- 0x1; }
+{ *l(r0) <- r1; }
+{ b r28; }
+
+// Note: the functions here don't save any registers. Beware of that if
+// you update anything here, or try to use these functions elsewhere.
+read_packet:
+  { r3 <- 15; r10 <- r31; } // Note: one less than the number of bytes to read.
+  { bl write_uart; r0 <- r27; }
+  read_packet_loop:
+    { bl read_uart; }
+    { *b(r28) <- r0; r28 <- r28 + 1; r3 <- r3 - 1; p1 <- r3 == 0; }
+    { !p1? b read_packet_loop; }
+    { b r10 + 1; }
+
+read_uart:
+  { r11 <- 0b00100; }
+  { *l(r29 + 4) <- r11; } // clear the rxc bit
+  uart_read_loop:
+    { r0 <- *b(r29 + 4); }
+    { p0 <- r0 & 0b1000; } // rxc
+    { !p0? b uart_read_loop; }
+  { b r31 + 1; r0 <- *b(r29); }
+
+write_uart:
+  { r11 <- 0b00100; }
+  { *l(r29 + 4) <- r11; } // clear txc
+  { *l(r29) <- r0; }
+  uart_write_loop:
+    { r0 <- *b(r29 + 4); }
+    { p0 <- r0 & 0b1; } // txc
+    { !p0? b uart_write_loop; }
+  { b r31 + 1; }
---
+++ boot/serial_loader/src/main.rs
@@ -0,0 +1,126 @@
+/*
+Quick-and-dirty client for the first-stage serial bootloader.
+Doesn't do anything resembling reasonable error handling, or
+verification of the downloaded program.
+*/
+
+use serial;
+use serial::posix::TTYPort;
+use serial::prelude::*;
+use std::io::{Read, Write};
+use std::io::ErrorKind::TimedOut;
+use std::fs::File;
+
+pub fn get_port() -> TTYPort {
+    let port = serial::open("/dev/ttyUSB0");
+    let mut port = match port {
+        Ok(port) => port,
+        Err(err) => { println!("Error opening port: {}", err);
+                      ::std::process::exit(1); }
+    };
+    port.reconfigure(&|settings| {
+        settings.set_baud_rate(serial::Baud115200)?;
+        settings.set_char_size(serial::Bits8);
+        settings.set_flow_control(serial::FlowNone);
+
+        Result::Ok(())
+    }).unwrap();
+
+    port
+}
+
+fn readchar(port: &mut TTYPort) -> u8 {
+    let mut buf = [0; 1];
+    loop {
+        let res = port.read(&mut buf);
+        match res {
+            Err(e) if e.kind() == TimedOut => {},
+            Err(e) => {
+                println!("{:?}", e);
+            }
+            Ok(l) if l > 0 => {
+                return buf[0];
+            },
+            _ => { },
+        }
+    }
+}
+
+fn wait_header(port: &mut TTYPort) {
+    // This isn't particularly efficent, but that doesn't
+    // particularly matter.
+
+    let mut buf: Vec<u8> = vec!();
+    let target = b"MBOOT";
+
+    println!("Waiting for bootloader...");
+
+    loop {
+        let c = readchar(port);
+        buf.push(c);
+        while &target[0..buf.len()] != &buf[..] {
+            buf.remove(0);
+        }
+        if buf == target {
+            return;
+        }
+    }
+}
+
+fn load_program(fname: &str) -> Vec<u8> {
+    println!("Loading program from {}...", fname);
+
+    let mut file = File::open(fname).unwrap();
+    let mut res = Vec::new();
+
+    file.read_to_end(&mut res).unwrap();
+    assert!(res.len() % 16 == 0, "Program length must be a multiple of 16 bytes");
+    let packets = res.len() / 16;
+    println!("Loaded {} bytes ({} packets)", res.len(), packets);
+
+    res
+}
+
+fn print_packet(packet: &[u8]) {
+    for i in 0..packet.len() {
+        print!("{:02x} ", packet[i]);
+        if i % 4 == 3 {
+            print!(" ");
+        }
+    }
+}
+
+fn send_program(port: &mut TTYPort, prog: &[u8]) {
+    let num_packets = prog.len() / 16;
+    port.write(&[num_packets as u8]).unwrap();
+
+    let mut remaining_packets = num_packets as u8;
+    for i in 0..num_packets {
+        remaining_packets -= 1;
+        assert_eq!(readchar(port), remaining_packets);
+
+        print!("Writing packet {}: ", i);
+        print_packet(&prog[i * 16..(i+1)*16]);
+        println!("");
+        port.write(&prog[i * 16..(i+1)*16]).unwrap();
+    }
+
+    assert_eq!(readchar(port), b'D');
+    assert_eq!(readchar(port), b'O');
+    assert_eq!(readchar(port), b'N');
+    assert_eq!(readchar(port), b'E');
+    println!("Done!");
+}
+
+fn main() -> Result<(), ::std::io::Error> {
+    let prog_filename = ::std::env::args().skip(1).next().expect("Specify a program file");
+    let prog = load_program(&prog_filename);
+
+    let mut port = get_port();
+
+    wait_header(&mut port);
+    println!("Got bootloader ping. Loading program...");
+    send_program(&mut port, &prog);
+
+    Ok(())
+}
