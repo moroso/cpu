3878a9bd2a31b74fdec5b4c5706da6411660c0f0
Michael Sullivan <sully@msully.net>
2020-08-29 00:10:26 -0700

    Make some code worse so I don't have to update my toolchain

--- sim/Stim_MCPU_MEM_il1c.cpp
+++ sim/Stim_MCPU_MEM_il1c.cpp
@@ -4,23 +4,25 @@
 
 void Stim_MCPU_MEM_il1c::read(uint32_t addr) {
   q.push({
-        .addr = addr >> 4,
-        .wait_ready = true,
-        .is_pause = false,
+        addr >> 4,
+        true,
+        false,
     });
 }
 
 void Stim_MCPU_MEM_il1c::read_nowait(uint32_t addr) {
   q.push({
-        .addr = addr >> 4,
-        .wait_ready = false,
-        .is_pause = false,
+        addr >> 4,
+        false,
+        false,
     });
 }
 
 void Stim_MCPU_MEM_il1c::pause() {
   q.push({
-      .is_pause = true,
+      0,
+      false,
+      true,
     });
 }
 
